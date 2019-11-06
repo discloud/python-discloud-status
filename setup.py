48a247748eb7d5c96d2243e9892e6e1ddb3fa90e
Algueem <40307829+Algueem@users.noreply.github.com>
2019-11-06 11:17:21 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='discloud',
           "Development Status :: 5 - Production/Stable",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8"
+          "Programming Language :: Python :: 3.8",
           "License :: OSI Approved :: MIT License",
           "License :: OSI Approved :: Apache Software License",
           "Operating System :: POSIX :: Linux"
