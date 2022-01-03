3785124058c805fec9310baeaec9bef6cdd5c9a6
Algueem <iagobr131313@gmail.com>
2022-01-03 15:49:07 -0300

    Update utils.py
    Visualizar termino de planos

--- discloud/utils.py
+++ discloud/utils.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 import datetime
-from datetime import datetime
-# from dateutil
 
 
 class Missing:
@@ -25,53 +23,57 @@ class InvalidArgument(DisCloudException):
 
 MISSING = Missing()
 
-EN_TEXTS = (
 
-    (86400, "day"),
-    (3600, "hour"),
-    (60, "minute"),
-    (1, "second")
-)
-
-PT_TEXTS = (
-    (86400, "dia"),
-    (3600, "hora"),
-    (60, "minuto"),
-    (1, "segundo")
-)
+def date_now() -> datetime.datetime:
+    return datetime.datetime.now(tz=datetime.timezone.utc)
 
 
 class TimePeriod:
-    def __init__(self, time: dict, language: str = MISSING):  # todo
-        self.language = "en" if language is MISSING else language
-        self.time = time
+    def __init__(self, data: dict) -> None:
+        self.language: str = data['lang']
+        left: dict = data['lastDataLeft']
+        self.left = left
+        self.days: int = left.get('days')
+        self.hours: int = left.get('hours')
+        self.minutes: int = left.get('minutes')
+        self.seconds: int = left.get('seconds')
 
-    def __str__(self):  # todo
+    def __str__(self) -> str:
         return self.format_string()
 
+    def __repr__(self) -> str:
+        return "<TimePeriod days=%s hours=%s minutes=%s seconds=%s>"
+
     def format_string(self):
-        texts = EN_TEXTS if self.language == "en" else PT_TEXTS
-        t = self.time
-        total = t['days']*86400 + t['hours']*3600 + t['minutes']*60 + t['seconds']
-        result = []
-        for amt, unit in texts:
-            if total > amt:
-                result.append()
-        return ', '.join(result)
+        data = self.left
+        if self.language == "pt":
+            return "{days} dias, {hours} horas, {minutes} minutos e {seconds} segundos".format(**data)
+        return "{days} days, {hours} hours, {minutes} minutes and {seconds} seconds".format(**data)
 
 
 class FutureDate:
-    def __init__(self, date: datetime) -> None:
+    def __init__(self, date: datetime.datetime, tz: datetime.tzinfo = None) -> None:
         self.date = date
+        self.tz = tz
 
-    def __repr__(self) -> datetime:
+    def __repr__(self) -> datetime.datetime:
         return self.date
 
-    def __str__(self) -> datetime:
-        return self.date
+    def __str__(self) -> str:
+        return self.date.strftime("%d-%m-%Y %H:%M:%S")
 
     @classmethod
-    async def from_string(cls, text: str) -> FutureDate:
-        d: datetime = datetime.datetime.strptime(text[:19], "%Y-%M-%DT%H:%M%S")
+    def from_string(cls, text: str) -> FutureDate:  # todo: refactor this function
+        d: datetime = datetime.datetime.strptime(text[:19], "%Y-%m-%DT%H:%M:%S")
         d.replace(tzinfo=None).astimezone(datetime.timezone.utc)
         return cls(d)
+
+    @classmethod
+    def from_dict(cls, data: dict, tz: datetime.tzinfo = None) -> FutureDate:
+        tz = tz if tz else datetime.timezone.utc
+        end_time = date_now() + datetime.timedelta(**data)
+        return cls(end_time, tz)
+
+    def to_tz(self, tz: datetime.tzinfo = None) -> FutureDate:
+        self.date.replace(tzinfo=self.tz).astimezone(tz)
+        return self
