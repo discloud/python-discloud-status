2f4fd6208f53bc32c42db6948b9427616f36a842
Algueem <iagobr131313@gmail.com>
2022-01-03 15:49:32 -0300

    Fix request
    corrigido não retornar nada no request

--- discloud/manager.py
+++ discloud/manager.py
@@ -18,27 +18,27 @@ class Route:
 class RequestManager:
     def __init__(self, api_token: str) -> None:
         self.api_token = api_token
-        self.session = aiohttp.ClientSession()
+        self.__session = aiohttp.ClientSession()
         self.cache: dict = {}
 
     async def request(self, route: Route) -> Any:
         method: str = route.method
         url: str = route.url
         headers: dict = {"api-token": self.api_token}
-        async with self.session as ses:
+        async with self.__session as ses:
             async with ses.request(method, url, headers=headers) as response:
-                return response
+                return await response.json()
 
-    async def fetch_bot(self, bot_id: int): # todo: typehint
-        await self.request(Route("POST", "/bot/{bot_id}", bot_id=bot_id))
+    async def fetch_bot(self, bot_id: int):  # todo: typehint
+        return await self.request(Route("GET", "/bot/{bot_id}", bot_id=bot_id))
 
-    async def fetch_user(self): # todo: typehint
-        await self.request(Route("POST", "/user"))
+    async def fetch_user(self):  # todo: typehint
+        return await self.request(Route("GET", "/user"))
 
-    async def restart(self, bot_id: int): # todo: typehint
-        await self.request(Route("POST", "/bot/{bot_id}/restart", bot_id=bot_id))
+    async def restart(self, bot_id: int):  # todo: typehint
+        return await self.request(Route("POST", "/bot/{bot_id}/restart", bot_id=bot_id))
 
-    async def fetch_logs(self, bot_id: int): # todo: typehint
+    async def fetch_logs(self, bot_id: int):  # todo: typehint
         ...
 
 
