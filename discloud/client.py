eea0c9ad7772c69eb416af11a60a24fb454249a4
Algueem <iagobr131313@gmail.com>
2022-01-03 15:52:19 -0300

    Update client.py
    Typehint + language support change

--- discloud/client.py
+++ discloud/client.py
@@ -7,13 +7,15 @@ class UserClient:
     def __init__(self, api_token: str, language: str = MISSING) -> None:
         self.api_token = api_token
         if language not in ["en", "pt"]:
-            raise InvalidArgument("Input a valid value") # todo: change error
+            raise InvalidArgument("Input a valid value")  # todo: change error
         self.language = "en" if language is MISSING else language
         self.__requester = RequestManager(api_token)
 
-    async def fetch_user_info(self):  # todo: typehint
+    async def fetch_user_info(self) -> DisCloudUser:
         data = await self.__requester.fetch_user()
-        return DisCloudUser(self, data)
+        data['lang'] = self.language
+        # print(data) debugging purposes
+        return DisCloudUser(data)
 
     async def fetch_bot(self, bot_id: int):  # todo: typehint
         ...
