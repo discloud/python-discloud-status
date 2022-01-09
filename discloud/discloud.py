2d45bde2cd5d2ddde1c1f928d0b8c693cfd1d4ef
Algueem <iagobr131313@gmail.com>
2022-01-09 19:13:49 -0300

    Support for botinfo + refactor user

--- discloud/discloud.py
+++ discloud/discloud.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 import enum
+import io
+from typing import TYPE_CHECKING
 
-from .utils import TimePeriod, FutureDate
+from .utils import TimePeriod, FutureDate, MemoryInfo
 
+if TYPE_CHECKING:
+    from .client import Client
 
 PT_TRANSLATIONS = {
     "Free": "Grátis",
@@ -12,33 +16,72 @@ PT_TRANSLATIONS = {
 }
 
 
+class Action:
+    def __init__(self, data: dict) -> None:  # todo: translate messages
+        self.message: str = data['message']
+        self.status: bool = data['status']
+
+    def __repr__(self) -> str:
+        return "<Message=\"%s\" status=%s>" % (self.message, self.status)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class File:
+    def __init__(self, fp: str | io.IOBase) -> None:
+        if not fp.endswith(".zip"):
+            raise ValueError("File must be .zip")   # todo: add custom error translations
+        self.fp = open(fp, "rb")
+        self.filename: str = self.fp.name
+
+
 class PlanType(enum.Enum):
     Free = 0
     Carbon = 3
     Gold = 1
     Platinum = 2
     Diamond = 4
-    # Ruby = 5 todo: v2 release
+    # ... = 5 todo: v2 release
     Sapphire = 6
-    # Krypton = 7 todo: v2 release
+    # ... = 7 todo: v2 release
 
 
-class Plan:
+class Logs:
     def __init__(self, data: dict) -> None:
+        self.url = data['link']
+        self.logs = self.text = data['logs']
+
+    def __str__(self) -> str:
+        return self.logs
+
+
+class BaseModel:
+    __slots__ = (
+        '_client'
+    )
+
+    def __init__(self, client: Client):
+        self._client = client
+
+
+class Plan(BaseModel):
+    def __init__(self, client: Client, data: dict) -> None:
+        super().__init__(client)
         self.type: PlanType = PlanType[data['plan']]
-        self.lifetime = data.get('planDataEnd') == "Lifetime"
-        self.language: str = data['lang']
+        self.lifetime: bool = data.get('planDataEnd') == "Lifetime"
+        self.language: str = client.language
         if self.lifetime:
-            self.expires_in = "never"
+            self.expires_in: str = "never"
             if self.language == "pt":
-                self.expires_in = "nunca"
+                self.expires_in: str = "nunca"
             self.expire_date = None
 
         else:
             self.expire_date: FutureDate = FutureDate.from_dict(data['lastDataLeft'])
-            self.expires_in: TimePeriod = TimePeriod(data)
+            self.expires_in: TimePeriod = TimePeriod(self._client, data)
 
-    def __str__(self) -> str:  # todo
+    def __str__(self) -> str:
         if self.language == "pt":
             return PT_TRANSLATIONS.get(self.type.name, self.type.name)
         return self.type.name
@@ -47,8 +90,48 @@ class Plan:
         return "<Plan type=%s>" % self.type
 
 
-class DisCloudUser:
-    def __init__(self, data: dict) -> None:
-        self.id = int(data['userID'])
-        self.plan = Plan(data)
+class User(BaseModel):
+    __slots__ = ("id",
+                 "plan")
 
+    def __init__(self, client: Client, data: dict) -> None:
+        super().__init__(client)
+        self.id: int = int(data['userID'])
+        self.plan: Plan = Plan(client, data)
+
+
+class BaseApplication(BaseModel):  # todo: v2 future site applications
+    async def restart(self):
+        raise NotImplementedError
+
+
+class Site(BaseApplication):  # todo: v2 discloud
+    async def restart(self):
+        ...
+
+
+class Bot(BaseApplication):
+    __slots__ = ("id",
+                 "status",
+                 "cpu",
+                 "memory",
+                 "last_restart")
+
+    def __init__(self, client: Client, data: dict) -> None:
+        super().__init__(client)
+        self.id: int = int(data['bot_id'])
+        self.status: str = data['container']
+        self.cpu: str = data['cpu']
+        self.memory: MemoryInfo = MemoryInfo(data['memory'])
+        self.last_restart: TimePeriod = TimePeriod.from_text(client, data['last_restart'])
+        # self.language = "Python"
+        # self.lang_version = "3.10.1"
+
+    async def restart(self) -> Action:
+        return await self._client.restart_bot(self.id)
+
+    async def fetch_logs(self) -> Logs:
+        return await self._client.fetch_logs(self.id)
+
+    async def commit(self, file: File, restart: bool = False) -> Action:
+        return await self._client.commit(self.id, file, restart=restart)
